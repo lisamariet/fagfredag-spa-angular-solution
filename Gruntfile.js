f0310b36d1854ab566fa723f0eb1f6936f3443eb
Torstein Bjørnstad <tosh@tosh.no>
2013-10-26 12:49:21 +0200

    Added more symbols, added simple proxy

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,34 @@
 'use strict';
 
 module.exports = function (grunt) {
+  var http = require('http'),
+    to_json = require('xmljson').to_json;
+
+  var proxy = function (req, res, next){
+    if(req.url.indexOf('/yr/') == 0){
+      var body = '';
+      http.get({
+        host: 'www.yr.no',
+        path: req.url.slice(3).replace('json','xml')
+      }, function(xmlRes){
+        xmlRes.on('data', function(chunk){
+          body += chunk;
+        });
+        xmlRes.on('end', function (){
+          to_json(body, function (error, data){
+            res.writeHead(200, {
+              'Content-Type': 'application/json; charset=utf-8',
+              'Access-Control-Allow-Origin': 'http://localhost'
+            });
+            res.end(JSON.stringify(data));
+          });
+        });
+      });
+    }else{
+      next();
+    }
+  }
+
   require('load-grunt-tasks')(grunt);
 
   grunt.initConfig({
@@ -26,10 +54,14 @@ module.exports = function (grunt) {
       livereload: {
         options: {
           open: true,
-          base: [
-            'app'
-          ],
-          livereload: 35729
+          base: 'app',
+          livereload: 35729,
+          middleware: function(connect, options){
+              return [
+                proxy,
+                connect.static(options.base)
+              ];
+          }
         }
       }
     }
